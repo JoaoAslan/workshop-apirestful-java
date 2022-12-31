442a988e86e0165f78c1a6ea1aec376e5255f8be
JoaoAslan <contato.joaoaslan@gmail.com>
2022-12-31 05:20:29 -0300

    User insertion with POST

--- src/main/java/com/educandoweb/springbootmongodb/services/UserService.java
+++ src/main/java/com/educandoweb/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.springbootmongodb.domain.User;
+import com.educandoweb.springbootmongodb.dto.UserDTO;
 import com.educandoweb.springbootmongodb.repositories.UserRepository;
 import com.educandoweb.springbootmongodb.resources.exceptions.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
