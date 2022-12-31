32a0045ab6eae6cc9dff83ba3233bbfd96cac0c4
JoaoAslan <contato.joaoaslan@gmail.com>
2022-12-31 19:17:50 -0300

    Simple query with @Query

--- src/main/java/com/educandoweb/springbootmongodb/repositories/PostRepository.java
+++ src/main/java/com/educandoweb/springbootmongodb/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.educandoweb.springbootmongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.educandoweb.springbootmongodb.domain.Post;
 
@@ -11,4 +12,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
     // https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
     // https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/#preface
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    // https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 }
