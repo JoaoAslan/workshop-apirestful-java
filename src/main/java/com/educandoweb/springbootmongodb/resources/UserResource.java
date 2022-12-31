840351b77804bc8ec8d1723f460d0b2c3cdc02e5
JoaoAslan <contato.joaoaslan@gmail.com>
2022-12-30 23:04:19 -0300

    Using DTO pattern to return users

--- src/main/java/com/educandoweb/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/educandoweb/springbootmongodb/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.springbootmongodb.domain.User;
+import com.educandoweb.springbootmongodb.dto.UserDTO;
 import com.educandoweb.springbootmongodb.services.UserService;
 
 @RestController
@@ -19,8 +20,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).toList();
+        return ResponseEntity.ok().body(listDTO);
     }
 }
