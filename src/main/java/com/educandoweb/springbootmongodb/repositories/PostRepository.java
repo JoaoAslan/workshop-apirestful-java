4767049be0bb2edf02db10647ab30f8aff0b7827
JoaoAslan <contato.joaoaslan@gmail.com>
2022-12-31 19:12:16 -0300

    Simple query with query methods

--- src/main/java/com/educandoweb/springbootmongodb/repositories/PostRepository.java
+++ src/main/java/com/educandoweb/springbootmongodb/repositories/PostRepository.java
@@ -1,9 +1,14 @@
 package com.educandoweb.springbootmongodb.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.educandoweb.springbootmongodb.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    // https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+    // https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/#preface
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
